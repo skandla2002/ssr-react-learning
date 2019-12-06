9a04f40a27255cd755b505276508c4dbe8db1b98
skandla2002 <skandla2002@gmail.com>
2019-12-06 23:43:33 +0900

    20서버 사이드 렌더링 - 스크립트로 스토어 초기 상태 주입하기

--- src/index.server.js
+++ src/index.server.js
@@ -21,7 +21,7 @@ const chunks = Object.keys(manifest.files)
   .map(key => `<script src="${manifest[key]}"></script>`) // 스크립트 테그로 변환하고
   .join(""); //합침
 
-function createPage(root) {
+function createPage(root, stateScript) {
   return `<!DOCTYPE html>
   <html lang="en">
   <head>
@@ -40,6 +40,7 @@ function createPage(root) {
     <div id="root">
       ${root}
     </div>
+    ${stateScript}
     <script src="${manifest["runtime-main.js"]}"></script>
     ${chunks}
     <script src="${manifest["main.js"]}"></script>
@@ -78,10 +79,14 @@ const serverRender = (req, res, next) => {
   }catch(e){
     return res.status(500);
   }
-  preloadContext.done = true;
+  preloadContext.done = true; 
   const root = ReactDOMServer.renderToString(jsx); // 렌더링을 하고
   // res.send(root); // 클라이언트에게 결과물을 응답합니다.
-  res.send(createPage(root)); // 클라이언트에게 결과물을 응답합니다.
+  // JSON을 문자열로 변환하고 악성 스크립트가 실행되는 것을 방지하기 위해 <를 치환처리
+  // https://redux.js.org/recipes/server-rendering#security-considerations
+  const stateString = JSON.stringify(store.getState().replace(/</g, '\\u003c'));
+  const stateScript = `<script>__PRELOADED_STATE__= ${stateString}</script>`; // 리덕스 초기 상태를 스크립트로 주입합니다.
+  res.send(createPage(root, stateScript)); // 클라이언트에게 결과물을 응답합니다.
 };
 
 const serve = express.static(path.resolve("./build"), {
